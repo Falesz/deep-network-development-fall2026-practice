a1a3d863bddadb26b6d365a5aeb8ce29c2cdee52
Falesz <jnjwrg@inf.elte.hu>
2026-09-15 06:57:27 +0200

    Create a rudimentary skeleton code for linear regression

---
+++ linear_regression/linear_regression.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+
+# Load the dataset
+data = pd.read_csv("linear_data.csv")
+
+# Create pytorch tensors
+raise NotImplementedError("Create pytorch tensors")
+
+# Create a model
+
+# Define loss function and optimizer
+
+# Train the model
+
+# Test the model
+
+# Persist the model
